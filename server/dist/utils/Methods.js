49dd18b293510ba4d29b0b919501382c57931077
Kian Merchant <kian@supesu.dev>
2022-03-09 14:59:21 +1300

    [0.0.1]: Remove dist and web

--- server/dist/utils/Methods.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.compileTriggers = void 0;
-const compileTriggers = (triggers) => {
-    return triggers.map((t) => t.replace("{PREFIX}", process.env.PREFIX));
-};
-exports.compileTriggers = compileTriggers;
-//# sourceMappingURL=Methods.js.map
