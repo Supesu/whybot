93a69375769db2b7c9753542537da9b69ee1d422
スペース <59186190+Supesu@users.noreply.github.com>
2022-06-09 03:05:42 +1200

    oops haha

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,4 @@
-export const __prod__ = false;
+export const __prod__ = true;
 
 export const API_PROTOCOL = __prod__ ? "https" : "http";
 export const API_URL = __prod__ ? "whybotapi.supesu.dev" : "localhost:4040";
